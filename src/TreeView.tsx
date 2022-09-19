d7efce6d249220c2e25e9918dd5971c921f70252
François Levasseur <francois_levasseur@hotmail.com>
2022-09-19 14:39:13 -0400

    fix: labels are centered with node circle

--- src/TreeView.tsx
+++ src/TreeView.tsx
@@ -1,7 +1,7 @@
 import './TreeView.css'
 import { useState } from 'react'
 import Tree from 'react-d3-tree'
-import { RawNodeDatum } from 'react-d3-tree/lib/types/common'
+import { CustomNodeElementProps, RawNodeDatum } from 'react-d3-tree/lib/types/common'
 import { tree } from './parser'
 
 type TreeViewProps = {
@@ -25,6 +25,31 @@ const toD3Tree = (tree: tree.topdown.TopDownTree): RawNodeDatum[] => {
   return tree.map(toD3Node)
 }
 
+const renderForeignObjectNode = ({ nodeDatum }: CustomNodeElementProps) => {
+  const isLeaf = !nodeDatum.children?.length
+
+  const x = -nodeDatum.name.length * 6
+  const y = isLeaf ? 5 : -40
+
+  const width = nodeDatum.name.length * 15
+  const height = 30
+
+  const svgProps: React.SVGProps<SVGForeignObjectElement> = {
+    x,
+    y,
+    width,
+    height,
+  }
+
+  const className = isLeaf ? 'node__leaf' : 'node__branch'
+  return (
+    <g className={className}>
+      <circle></circle>
+      <foreignObject {...svgProps}>{<div className="node__label">{nodeDatum.name}</div>}</foreignObject>
+    </g>
+  )
+}
+
 export const TreeView = (props: TreeViewProps) => {
   const [x, setX] = useState<HTMLDivElement | null>(null)
   const { width, height } = x?.getBoundingClientRect() ?? { width: 0, height: 0 }
@@ -41,7 +66,7 @@ export const TreeView = (props: TreeViewProps) => {
         branchNodeClassName="node__branch"
         leafNodeClassName="node__leaf"
         separation={{ siblings: 1, nonSiblings: 1 }}
-        nodeSize={{ x: 150, y: 150 }}
+        renderCustomNodeElement={(rd3tProps: CustomNodeElementProps) => renderForeignObjectNode({ ...rd3tProps })}
       />
     </div>
   )
